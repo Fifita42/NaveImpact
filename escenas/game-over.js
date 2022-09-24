f0d402c6479cf28eaf1b494909ca5992d6bf7cf2
Tobias Molina <102197218+Fifita42@users.noreply.github.com>
2022-09-23 22:09:12 -0300

    Update game-over.js

--- escenas/game-over.js
+++ escenas/game-over.js
@@ -5,11 +5,6 @@ export class Gameover extends Phaser.Scene//exporto una escena
         super({ key: 'gameover' });//nombre clave de esta escena
         this.restartButton = new RestartButton(this);
     }
-    preload()
-    {
-        this.load.image('gameover', '../imagenes/gameover.png');
-        this.restartButton.preload();
-    }
     create()
     {
         this.add.image(350,305,'bg').setScale(2);//como ya se preload el background en game, no hace falta repetir
