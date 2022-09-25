1bba0b62044758a89008fb4035bb491c58f8d9c5
Tobias Molina <102197218+Fifita42@users.noreply.github.com>
2022-09-25 16:34:13 -0300

    Add files via upload

---
+++ componentes/mando/mando.js
@@ -0,0 +1,128 @@
+import {wid} from '../../index.js'
+import {heig} from '../../index.js'
+export class Mando
+{
+    constructor(scene, initialLives)
+    {
+        this.relatedScene = scene;
+        this.initialLives = initialLives;
+        this.widd;
+        this.heigg;
+        this.right = {isDown: false};
+        this.left = { isDown: false };
+        this.up = { isDown: false };
+        this.down = { isDown: false };
+        this.visibles;
+    }
+
+    create()
+    {
+        this.calc();
+        this.calcW();
+
+        this.rightButton = this.relatedScene.add.image(this.widd-150,this.heigg-230,'derecha').setInteractive({ draggable: true });this.rightButton.setOrigin(0,0);this.rightButton.setScale(2);
+        this.leftButton = this.relatedScene.add.image(this.widd-330,this.heigg-230,'izquierda').setInteractive({ draggable: true });this.leftButton.setOrigin(0,0);this.leftButton.setScale(2);
+        this.upButton = this.relatedScene.add.image(170,this.heigg-370,'izquierda').setInteractive({ draggable: true });this.upButton.angle = 90;this.upButton.setOrigin(0,0);this.upButton.setScale(2);
+        this.downButton = this.relatedScene.add.image(170,this.heigg-200,'derecha').setInteractive({ draggable: true });this.downButton.angle = 90;this.downButton.setOrigin(0,0);this.downButton.setScale(2);
+    
+        this.leftButton.on('pointerdown', () => {
+            this.left.isDown = true;
+            this.right.isDown = false;
+            
+          });
+          this.leftButton.on('pointerout', () => {
+            this.left.isDown = false;
+          });
+          this.leftButton.on('dragend', () => {
+            this.left.isDown = false;
+          });
+      
+
+          this.rightButton.on('pointerdown', () => {
+            this.right.isDown = true;
+            this.left.isDown = false;
+           
+          });
+          this.rightButton.on('pointerout', () => {
+            this.right.isDown = false;
+          });
+          this.rightButton.on('dragend', () => {
+            this.right.isDown = false;
+          });
+
+
+          this.upButton.on('pointerdown', () => {
+            this.down.isDown = false;
+            this.up.isDown = true;
+          });
+          this.upButton.on('pointerout', () => {
+            this.up.isDown = false;
+          });
+          this.upButton.on('dragend', () => {
+            this.up.isDown = false;
+          });
+
+
+          this.downButton.on('pointerdown', () => {
+            this.down.isDown = true;
+            this.up.isDown = false;
+          });
+          this.downButton.on('pointerout', () => {
+            this.down.isDown = false;
+          });
+          this.downButton.on('dragend', () => {
+            this.down.isDown = false;
+          });
+          if(!this.visibles)
+          {
+            this.rightButton.visible=false;
+            this.leftButton.visible=false;
+            this.downButton.visible=false;
+            this.upButton.visible=false;
+          }
+        
+
+    }
+    calc()
+    {
+      if(window.innerWidth>window.innerHeight){
+        this.visibles = false;
+        console.log('wid grande');
+      }else this.visibles = true;
+
+      if(wid>heig)
+        {
+          if(wid<=600)this.widd = wid;
+          else {
+            this.widd = 600;
+          }
+        }else
+        {
+            if(wid<=600)this.widd = wid;
+            else {
+              this.widd = 600;
+            }
+        }
+      
+    }
+
+    calcW()
+    {
+        if(heig>wid)
+        {
+            if(heig<=1200)
+            {
+                this.heigg = heig;
+            }
+            else
+            {
+                this.heigg = 800;
+            }
+        }
+        else
+        {
+            this.heigg = 800;
+        }
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+export let heig = window.innerHeight;
+export let wid = window.innerWidth;
+console.log(window.innerWidth);
+console.log(window.innerHeight);
+function calc()
+{
+  if(heig>wid)
+  {
+    if((heig*1.5)<=1200)
+    {
+      heig = heig*1.5;
+      return heig;
+    }else
+    {
+      return 800;
+    }
+  }else
+  {
+    heig = 800;
+    return 800;
+  }
+ // return heig = 791*1.5;
+}
+function calcW()
+{
+ // return wid = 412*1.5;
+  if(wid>heig)
+  {
+    if(wid<=600)return wid;
+    else {
+      wid = 600;
+      return 600;
+    }
+  }else
+  {
+    if((wid*1.5)<=700)
+    {
+      wid = wid*1.5;
+      return wid;
+    }else return 600;
+
+  }
+}
+import { Game } from './game.js';//trae la clase Game dentro del archivo especificado
+import { Congratulations  } from './escenas/congratulations.js';
+import { Gameover } from './escenas/game-over.js';
+
+const config = {
+  pixelArt: true,//remarca los pixeles de las imagenes
+  type: Phaser.AUTO,//indica si usar canvas o WebGl depende de ordenador
+  // width: 600,//tamaño de canvas
+  // height: 800,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'phaser-example',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: calcW(),
+    height: calc(),
+},
+  parent: 'container',//id del contenedor
+  backgroundColor: '#34495E',//fondo del lienzo
+  banner:
+  {
+    hidePhaser: true,//oculta texto de phaser en la consola
+    text:'#000000',//cambia color del titulo del juego enn la consola
+    background:[
+      'red',
+      'yellow',
+      'red',
+      'transparent'
+    ]
+  },
+  scene: [Game, Congratulations, Gameover],//que escenas va a haber en mi juego [Game,GameOver,Win,etc]
+  physics: {//fisiacas
+    default: 'arcade',//las basicas de 2d
+    arcade: {
+      gravity: { y: 0},//solo gravedad en eje y
+      debug: false
+    }
+  }
+}
+
+var game = new Phaser.Game(config);//inicia la clase Game con las configuraciones dadas
