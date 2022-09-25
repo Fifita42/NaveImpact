e4234c23d8e44e84b94068162807ccb9c3614f8a
Tobias Molina <102197218+Fifita42@users.noreply.github.com>
2022-09-25 16:02:59 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,143 +1,212 @@
-import{Scoreboard} from './componentes/scoreboard/Scoreboard.js';
-import{LiveCounter} from './componentes/live/LiveCounter.js';
-import{Mando} from './componentes/mando/mando.js';
-import{Nave} from './componentes/nave/nave.js';
-import{Meteor, record} from './componentes/asteroides/asteroid.js';
-export let tiempo = 700;//cada cuamto caen asteroides
-export let caida = 222;//velocidad de caida
-export class Game extends Phaser.Scene {//permitimos enviar esta clase a quien lo pida
+import{PhaseConstructor} from './escenas/PhaseConstructor.js';
+import{Scoreboard} from './componentes/Scoreboard.js';//traigo otro script y lo incluyo
+import{LiveCounter} from './componentes/LiveCounter.js';//traigo otro script y lo incluyo
 
-//Phaser.Scene contiene una escena básica y nosotros la extendemos para darle la funcionalidad que requiere nuestro juego.
+import { Platform } from './componentes/nave.js';
+import { Ball } from './componentes/ball.js';
+
+
+const INITIAL_LIVES = 3;
+const INITIAL_VELOCITY_X = -60;//velocidad inicial en x de la bala
+
+window.addEventListener('keydown',(e)=>//la cree para reiniciar la pagina con enter
+{
+  if(e.key=='Enter')location.reload();
+});
+
+
+export class Game extends Phaser.Scene {
     constructor() {
       super({ key: 'game' });//nombre clave de esta escena
     }
-    init()
-    {
-      this.naveP = new Nave(this);
-      this.meteoros = new Meteor(this);
-      this.score = new Scoreboard(this);
-      this.lives = new LiveCounter(this,3);
-      this.mado = new Mando(this);
+    init() {//se ejecuta solo una vez al inicio
+        this.glueRecordVelocityX = INITIAL_VELOCITY_X;//guarda la velocidad de la bala para cuando se pega a la nave
+        this.phaseConstructor = new PhaseConstructor(this);//instancio el constructor de los niveles
+        this.scoreboard = new Scoreboard(this);//instancio una nueva clase scoreboard con todos los datos de esta escena
+        this.liveCounter = new LiveCounter(this, INITIAL_LIVES);//Esto permite que, cada vez que la escena del juego se resetea, se cree un nuevo contador de vidas.
+        this.naveP = new Platform(this);
+        this.bullet = new Ball(this);
+      }
+    preload() {//precarga cosas
+      let url = 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js';//para usar el joistick
+      this.load.plugin('rexvirtualjoystickplugin', url, true);//impplementa un joystick
+      this.load.image('background', 'images/background.jpg');//carga la imagen y le da nombre background
+      //this.load.image('bullet', 'images/bullet/b1.png');
+      this.load.image('bluebrik', 'images/brickBlue.png');
+      this.load.image('blackbrik', 'images/brickBlack.png');
+      this.load.image('greenbrik', 'images/brickGreen.png');
+      this.load.image('orangebrik', 'images/brickOrange.png');
+      this.load.image('yellowbrick', 'images/brickYellow.png');
+      this.load.image('greybrik', 'images/brickGrey.png');
+      this.load.image('naveP', 'images/NaveAnim/nav1.png');
+      this.load.audio('laser', 'sonidos/laser.mp3');
+      this.load.atlas('bulletTot','images/bullet/bulletTot.png','images/bullet/bulletTot.json');//sprite
+      this.load.spritesheet('bluediamond',
+      'images/diamondSprite.png',
+      { frameWidth: 48, frameHeight: 48 }
+);
     }
-    preload() 
-    {
-      let url = 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js';
-      this.load.plugin('rexvirtualjoystickplugin', url, true);
-      this.load.image('bg','imagenes/background.jpg');
-      this.load.image('cuerpo','imagenes/cuerpo.png');
-      this.load.image('asteroide','imagenes/asteroid.png');
-      this.load.image('derecha','imagenes/right.png');
-      this.load.image('izquierda','imagenes/left.png');
-      this.load.atlas('navecita','imagenes/cohete.png','sprite.json');
-      this.load.image('gameover', 'imagenes/gameover.png');
-      this.load.spritesheet('button','imagenes/restart.png',
-      {
-          frameWidth:190,
-          frameHeight:49
-      });
-
-    }
-    
+  
     create() {
-      this.physics.world.setBoundsCollision(true, true, true, true);
-      this.bg = this.add.image(650,0,'bg').setOrigin(0,0).setScale(1.5);
-      //this.bg.flipX = 180;
-      this.bg.angle = 90;
-      this.mado.create();
-      this.score.create();
-      this.lives.create();
+        this.add.image(410, 250, 'background');
+        this.physics.world.setBoundsCollision(true, true, true, false);//todo es fisico menos el suelo
+        this.liveCounter.create();
+        this.naveP.create();
+        
+        this.cursors = this.input.keyboard.createCursorKeys();
+        
+        //creo el joistick
+        this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+          x: 55,
+          y: 400,
+          radius: 100,
+          base: this.add.circle(0, 0, 50, 0x888888),
+          thumb: this.add.circle(0, 0, 25, 0xcccccc),
+          forceMin: 50,//fuerza minima hacia un lado para que se mueva
+    }).on('update', this.dumpJoyStickState, this);
 
-      this.anims.create(
-        {
-          key:'mov',
-          frames: this.anims.generateFrameNames('navecita',
+    this.joystickCursors = this.joyStick.createCursorKeys();//contiene la informaciion de direccion del cursor. up, right, left y down
+        
+        this.anims.create(
           {
-            prefix:'Sprite',
-            end: 6, 
-            zeroPad:4
-          }),
+            key:'balas',
+            frames: this.anims.generateFrameNames('bulletTot',
+            {
+              end: 6, 
+              //zeroPad:0
+            }),
+            frameRate: 10,
+            repeat: -1,
+            yoyo: true
+          });
+        this.bullet.create();
+        this.physics.add.collider(this.bullet.get(), this.naveP.get(), this.navePImpact, null, this);//.get es una funcion dentro de ambas que devuelve .bullet o .navp
+        this.scoreboard.create();
+        this.disparar = this.sound.add('laser');//creo la variable del sonido que puedo controlar
+        
+        this.anims.create({
+          key: 'bluediamondanimation',
+          frames: this.anims.generateFrameNumbers('bluediamond', { start: 0, end: 7 }),
           frameRate: 10,
           repeat: -1,
-          yoyo: true
-        });
+          yoyo: true,
+    });
+    this.anims.create({
+      key: 'reddiamondanimation',
+      frames: this.anims.generateFrameNumbers('bluediamond', { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1,
+      yoyo: true,
+    });
+    this.anims.create({
+      key: 'greendiamondanimation',
+      frames: this.anims.generateFrameNumbers('bluediamond', { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1,
+      yoyo: true,
+    });
+    this.phaseConstructor.create();
+    this.miSprite = this.physics.add.sprite(40, 40, 'bluediamond');
+    this.miSprite.anims.play('bluediamondanimation');
+  }
 
-        this.naveP.create();
-        this.imagen = this.add.sprite(this.naveP.get().x,this.naveP.get().y,'navecita');
 
-        this.meteoros.create();
-        this.meteoros.setAsteroidCollicion();
-
-      var timer = this.time.addEvent(//lanzar meteoros cada tiempo
-        {
-          delay:tiempo,
-          callback: this.lanzarMeteor,
-          callbackScope: this,
-          loop: true
-        })
-        this.cursors = this.input.keyboard.createCursorKeys();
-    }
     update() 
     {
-      this.imagen.x = this.naveP.get().x;
-      this.imagen.y = this.naveP.get().y-20;
-      this.imagen.anims.play('mov',true);
-      this.naveP.mover(this.cursors,this.mado);
+      this.naveP.updatePosition(this.bullet,this.cursors,this.joystickCursors);//mover
+        if (this.bullet.isLost()) {//se fue la bala?
+          let gameNotFinished = this.liveCounter.liveLost();//true o false
+          if(gameNotFinished)//se perdio la bala
+          {
+            this.naveP.setInitialState(this.bullet);//devolver todoa su posicion
+            this.naveP.setInitialSize();//devolver a su tamaño original
+            this.naveP.removeGlue();//desactiva la habilidad pegamento
+            this.glueRecordVelocityX = INITIAL_VELOCITY_X;
+          }
+        }
+
+        if (this.cursors.up.isDown||this.joystickCursors.up.isDown ) {//tecla ariba
+          if(this.bullet.isGlued)//la pelota esta aderida?
+          {
+            this.bullet.throw(INITIAL_VELOCITY_X);//lanzar la bala
+          }else if(this.naveP.isGluedBecausePower()){//devuelve true si la bala esta pegada por la habilidad
+            this.bullet.throw(this.glueRecordVelocityX);
+            this.naveP.hasBallGlued = false;
+          }
+        }
+        
     }
 
-    lanzarMeteor()
+    navePImpact(bullet,naveP)//llamado cada vez que la nave y la bala chocan
     {
-        let met1 = this.meteoros.get().get(Phaser.Math.Between(0,200),Phaser.Math.Between(-50,-200),'asteroide').setScale(0.3);
-        
-        let met2 = this.meteoros.get().get(Phaser.Math.Between(220,400),Phaser.Math.Between(-100,-150),'asteroide').setScale(0.3);
-        
-        let met3 = this.meteoros.get().get(Phaser.Math.Between(420,600),Phaser.Math.Between(-200,-250),'asteroide').setScale(0.3);
-    
-        if(met1&&met2&&met3)
-        {
-        met1.setActive(true);
-        met1.body.velocity.y = caida;
-  
-        met2.setActive(true);
-        met2.body.velocity.y = caida;
-  
-        met3.setActive(true);
-        met3.body.velocity.y = caida;
-      }
-      met1.outOfBoundsKill = true;
-      met2.outOfBoundsKill = true;
-      met3.outOfBoundsKill = true;
-      caida+=2;
-      this.score.incrementPoints(30);
-    }
-
-    impact(aster)
-    {
-      this.score.incrementPoints(-30);
-      caida+=10;
-      if(aster.y>this.naveP.get().y-30)
-      {
-        if(aster.x<this.naveP.get().x)
-          {
-
-          }
-          else if(aster.x>this.naveP.get().x)
-          {
-
-          }
-    }else
-      {
-        this.lives.liveLost();
+      this.disparar.play();
+      this.scoreboard.incrementPoints(1);//cada vez que choca, le envio un punto
+      let relativeImpact = bullet.x - naveP.x;//decide si toco mas a la izquierda o a la derecha- - es izq y + es derech.
+      
+      if(this.naveP.hasGluePower()) {//tiene la habilidad pegamento
+        bullet.setVelocityY(0);
+        bullet.setVelocityX(0);//mantiene la pelota unida a la nave
+        this.glueRecordVelocityX = this.calculateVelocity(relativeImpact);//cuando suelte la vola va a salir como si no se hubiese pegado
+        this.naveP.hasBallGlued = true;//actualmente tiene la bala pegada
+      } else {
+        bullet.setVelocityX(this.calculateVelocity(relativeImpact));//bota a la derecha o izquierda
       }
     }
 
-    
+    calculateVelocity(relativeImpact) {//rebota para derecha o izquierda
+      if(relativeImpact > 50) {
+        relativeImpact = 50;
+      }
+      if (relativeImpact > 0) {
+        return (8 * relativeImpact);
+      } else if (relativeImpact < 0) {
+        return (8 * relativeImpact);
+      } else {
+        return (Phaser.Math.Between(-10, 10))//suelta direccion al azar
+      }
+    }
+
+    brickImpact(bullet,brik)//destruyo los bloques
+    { 
+      brik.disableBody(true,true); 
+      this.scoreboard.incrementPoints(10);
+      
+      if (this.phaseConstructor.isPhaseFinished()) { //va a phaseConstructor.js al metodo isPhaseFinished que va al nivel respectivo que va a phase.js que va a isPhaseFinished que devuelve true if this.bricks.countActive() === 0
+        this.phaseConstructor.nextLevel();//pasa al siguiente nivel o le envia true a endGame para terminar el juego
+        this.naveP.setInitialState(this.bullet);//devuelve la nave a su posicion
+      }
+    }
+
   endGame(completed)
   {
-      if(completed)
+      if(!completed)//si es falso, perdio el juego
       {
-          caida = 222;
-          this.scene.start('gameover');
+          this.scene.start('gameover');//va a game-over.js
+      }
+      else//si es true, continua el juego
+      {
+          this.scene.start('congratulations');//va a congratulations.js
       }
   }
-    
+
+    increaseLives() {
+      this.liveCounter.increase();
+    }
+
+    setGluePower() {
+      this.naveP.setGluePower();
+    }
+
+    setPlatformBig() {
+      this.naveP.setBigSize();
+    }
+
+    removeGlueFromBall() {
+      this.bullet.removeGlue();
+    }
+    dumpJoyStickState() {//comprobar los resultados lanzados por el joystick
+      var cursorKeys = this.joyStick.createCursorKeys();
+      console.log('Force: ', this.joyStick.force);//me da la fuerza aplicada
+      console.log('Angle: ', this.joyStick.angle);//me da el aungulo
+    }
 }
